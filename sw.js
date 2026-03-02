fab73572cea08ca29a17b2dd2d15d982a5ece587
Iago Ferreira <iagosfs2007.vr@gmail.com>
2026-03-02 12:58:22 -0300

    push

---
+++ sw.js
@@ -0,0 +1,8 @@
+// sw.js - Service Worker básico
+self.addEventListener('install', (e) => {
+  console.log('Service Worker instalado!');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Necessário para ser instalável
+});
